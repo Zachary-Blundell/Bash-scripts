7ebf900e2a8427ee2d0092312cb91bb59832398b
Zachary <zacharyblundell@live.com>
2025-05-06 11:25:39 +0200

    formating

--- openProject.sh
+++ openProject.sh
@@ -10,29 +10,30 @@
 
 set -euo pipefail
 
-codeDir='/home/zaiquiri/Code'
+readonly codeDir='/home/zaiquiri/Code'
 
 if [[ ! -d "$codeDir" ]]; then
-  echo "code directory $codeDir does not exist"
+  echo "code directory $codeDir does not exist" >&2
   exit 1
 fi
 
-cd $codeDir
+cd ${codeDir}
 # find Project
-projectDir=$(
+readonly projectDir=$(
   fd --type d --max-depth 1 . | fzf --prompt='Projects> ' \
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
     --color=selected-bg:#45475a \
     --color=border:#313244,label:#cdd6f4 \
-    --border="rounded" --border-label="Code Projects" --border-label-pos="0" --preview-window="border-rounded" \
+    --border="rounded" --border-label="Code Projects" \
+    --border-label-pos="0" --preview-window="border-rounded" \
     --padding="2" --margin="10%" --marker="" \
     --pointer="🚀" --separator="─" --scrollbar="│" --layout="reverse" \
     --info="right"
 )
 
-if [ -n "$projectDir" ]; then
-  cd "$codeDir/$projectDir"
+if [ -n "${projectDir}" ]; then
+  cd "${codeDir}/${projectDir}"
   nvim .
 fi
