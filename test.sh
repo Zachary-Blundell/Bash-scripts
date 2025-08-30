a6acb5750a1d27718a5967f0f6459a528364c33b
Zachary Blundell <zacharyblundell@live.com>
2025-08-30 02:03:31 +0200

    removed

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CHATGPT="https://chatgpt.com/"
-OUTLOOK="https://outlook.live.com/mail/0/inbox/"
-DEEPL="https://www.deepl.com/en/translator#en/fr/"
-
-hyprctl keyword exec [workspace 2] "brave --new-window $CHATGPT $OUTLOOK $DEEPL"
